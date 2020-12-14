2907e1633dc4be2deec0269448b379848d357318
Omkar <omkar661997gmail.com>
2020-12-14 15:40:44 +0530

    New Changes

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'toDoList',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'MyProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'TasklistDatabse.sqlite3',
     }
 }
 
